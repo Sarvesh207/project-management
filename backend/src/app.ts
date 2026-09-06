3e84f22df27253aaf3414cecb15f39fc4e188371
Sarvesh207 <sarveshgaynar@gmail.com>
2026-09-06 16:24:11 +0530

    databse configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,29 @@
 import express from "express";
+import { pool } from "./db/pool";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  console.log("I am running ")
-  res.json({
-    success: true,
-    message: "App is running",
-  });
+
+
+app.get("/health", async (_req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      success: true,
+      message: "API and database are working",
+      databaseTime: result.rows[0].now,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Database connection failed",
+    });
+  }
 });
 
 export default app;
